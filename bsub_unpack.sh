ee9b12caa37c7fa92e5c452e3ac5ac4ad9f17f1c
Gurkirt Singh <gusingh@eu-login-44.euler.ethz.ch>
2021-03-04 17:38:09 +0100

    fix typo

--- bsub_unpack.sh
+++ bsub_unpack.sh
@@ -1,9 +1,9 @@
 echo "temp directory for the job" $TMPDIR
-TARGETDIR=${TMPDIR}/data/
+TARGET_DIR=${TMPDIR}/data/
 mkdir -p $TARGET_DIR
 echo  $TARGET_DIR
 SOURCE_DIR=/cluster/work/cvl/gusingh/data/ava/frames-tarballs/
-time python unpack_dataset.py ${TARGET_DIR} ${SOURCE_DIR} --num_jobs=8
+time python unpack_dataset.py ${TARGET_DIR} ${SOURCE_DIR} --num_jobs=16
 ls -lh ${TARGET_DIR}
 
-python tools/run_net.py --cfg config/AVA/SLOWFAT.yml AVA.FRAMES_DIR ${TARGET_DIR}
+#python tools/run_net.py --cfg config/AVA/SLOWFAT.yml AVA.FRAMES_DIR ${TARGET_DIR}
